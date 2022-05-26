9ac68299840d2da6116d6973e01f9c69c3ba3fed
Henri Hals <halshenri@gmail.com>
2022-05-26 12:27:07 +0200

    fin des exercices 3.1 et 3.2

---
+++ 3.1.js
@@ -0,0 +1,20 @@
+const readlineSync = require("readline-sync");
+
+var arr = [];
+
+var ch;
+
+var total = 0;
+
+var nbrefois = new Number (
+    readlineSync.question("Combien de chiffres souhaites-tu additionner ? ")
+);
+
+for (i = 0; i < nbrefois; i++) {
+    ch = new Number (readlineSync.question("Ecrit un chiffre : "));
+    arr.push(ch);
+    console.log(arr.join());
+    total += ch;
+}
+
+console.log("Le total des chiffres est de : " + total);
---
+++ 3.2.js
@@ -0,0 +1,24 @@
+const readlineSync = require("readline-sync");
+
+var arr = [];
+
+var ch;
+
+var total = 0;
+
+var moyenne = 0;
+
+var nbrefois = new Number (
+    readlineSync.question("Combien de chiffres souhaites-tu additionner ? ")
+);
+
+for (i = 0; i < nbrefois; i++) {
+    ch = new Number (readlineSync.question("Ecrit un chiffre : "));
+    arr.push(ch);
+    console.log(arr.join());
+    total += ch;
+}
+
+moyenne = total / nbrefois;
+
+console.log("La moyenne des chiffres est de : " + moyenne);
